2d9908eb86124c64ab252499f9642d1cee980301
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2021-12-05 17:34:19 -0500

    set up flags for each condition, created a method for a random length, and generate a string of that length

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,70 @@ var flags = [
 ];
 var minLength = 8;
 var maxLength = 128;
-var answer
+var lengthFinal;
+var answer;
+var password = [];
 
-for (var i = 0; i < flags.length; i++) console.log(flags[i]);
+// for (var i = 0; i < flags.length; i++) {
+//   switch(i) {
+//     case '0':
+//       window.prompt(Include Lowercase? (Y/N));
+//       break;
+// }
+//   }
+
+function getLength(min, max) {
+  console.log(min);
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 // Assignment code here
 function generatePassword() {
-  answer = window.prompt("Do you want lowercase characters?");
-  if (answer.toLowerCase == 'yes')  
-    flags[]
+  // console.log(flags[0]);
+  // answer = window.prompt("Do you want lowercase characters?");
+  // if (answer == "yes") {
+  //   flags[0] = true;
+  //   console.log(flags[0]);
+  // }
+
+  // answer = window.prompt("Do you want uppercase characters?");
+  // if (answer == "yes") {
+  //   flags[1] = true;
+  //   console.log(flags[1]);
+  // }
+
+  // answer = window.prompt("Do you want Numeric characters?");
+  // if (answer == "yes") {
+  //   flags[2] = true;
+  //   console.log(flags[2]);
+  // }
+
+  // answer = window.prompt("Do you want Special characters?");
+  // if (answer == "yes") {
+  //   flags[3] = true;
+  //   console.log(flags[3]);
+  // }
+
+  answer = window.prompt("What is the minimum length of the password?");
+  flags[4] = parseInt(answer);
+  console.log(flags[4]);
+
+  answer = window.prompt("What is the maximum length of the password?");
+  flags[5] = parseInt(answer);
+  console.log(flags[5]);
+
+  lengthFinal = getLength(flags[4], flags[5]);
+  console.log(lengthFinal);
+
+  for (i = 0; i < lengthFinal; i++) {
+    password.push("y");
+  }
+
+  for (i = 0; i < password.length; i++) {
+    console.log(password[i]);
+  }
 }
 
 // Get references to the #generate element
